41e8d32193e92b72655d5a0d7bcf7111a10fea2d
slmanasad <slman.me@gmail.com>
2018-07-24 02:45:09 +0500

    commit1

--- app/src/main/java/com/example/salman/restaurantapplication/AverageRating.java
+++ app/src/main/java/com/example/salman/restaurantapplication/AverageRating.java
@@ -4,6 +4,13 @@ package com.example.salman.restaurantapplication;
  * Created by Salman on 7/24/2018.
  */
 
+
+/**
+ * This class is used to convert Double to double, since I get null pointer exception so I create this class
+ * and I create object of this class and response.body() is used to set the value the private field in this class.
+ * and after that it will be converted to double.
+ * Double d = response.body() gives null pointer exception
+ */
 public class AverageRating {
     private Double rating;
 
